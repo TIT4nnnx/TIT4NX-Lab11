a123c85dce1aeb7fbbc8d2f90a9bc28e4e6ed687
Tittanx <ekarach6627@gmail.com>
2023-01-03 16:41:33 +0700

    TIT4NX Lab11_3

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,34 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    string textline;
+    int i = 0;
+    double num ;
+    double sum = 0;
+    double sum_sq = 0;
+    double mean = 0;
+    double sd ;
+
+    while(getline(source,textline)){
+        num = atof(textline.c_str());
+        sum += num;
+        sum_sq += pow(num,2);
+        i++; 
+    }
+    
+    mean = sum/i;
+
+    sd = sqrt((sum_sq/i)-pow(mean,2));
+
+    cout << "Number of data = " << i << "\n";
+    cout << setprecision(3);
+    cout << "Mean = "<< mean <<"\n";
+    cout << "Standard deviation = "<< sd ;
+}
